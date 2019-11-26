9f12c599c77d3dc514202cab7c12d2cf93b96388
Jared S <jaredjames2020@gmail.com>
2019-11-26 00:06:21 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/name' do
+    "My name is John!"
+  end
+  
+  get '/hometown' do
+    "My hometown is Japan."
+  end
+  
+  get '/favorite-song' do
+    "My favorite song is Happy Birthday."
+  end
+
 end
